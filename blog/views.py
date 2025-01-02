aaf05094109db5733647e079bfa76f8b63ace857
Abdou_Mourid <abdoumouhid96@yahoo.com>
2025-01-02 11:33:32 +0100

    Building a search view

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from .models import Post
 from django.views.generic import ListView
-from .forms import CommentForm, EmailPostForm
+from django.contrib.postgres.search import SearchVector
+from .forms import CommentForm, EmailPostForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
@@ -19,7 +20,7 @@ def post_list(request, tag_slug=None):
         post_list = post_list.filter(tags__in=[tag])
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
+    page_number = request.GET.get("page", 1)
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
@@ -28,14 +29,7 @@ def post_list(request, tag_slug=None):
     except EmptyPage:
         # If page_number is out of range get last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(
-        request,
-        'blog/post/list.html',
-        {
-            'posts': posts,
-            'tag': tag
-        }
-    )
+    return render(request, "blog/post/list.html", {"posts": posts, "tag": tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -45,7 +39,8 @@ def post_detail(request, year, month, day, post):
         slug=post,
         publish__year=year,
         publish__month=month,
-        publish__day=day)
+        publish__day=day,
+    )
 
     # List of active comments for this post
     comments = post.comments.filter(active=True)
@@ -53,23 +48,23 @@ def post_detail(request, year, month, day, post):
     form = CommentForm()
 
     # List of similar posts
-    post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(
-        tags__in=post_tags_ids
-    ).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(
-        same_tags=Count('tags')
-    ).order_by('-same_tags', '-publish')[:4]
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(
+        id=post.id
+    )
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
 
     return render(
         request,
-        'blog/post/detail.html',
+        "blog/post/detail.html",
         {
-            'post': post,
-            'comments': comments,
-            'form': form,
-            'similar_posts': similar_posts
-        }
+            "post": post,
+            "comments": comments,
+            "form": form,
+            "similar_posts": similar_posts,
+        },
     )
 
 
@@ -77,67 +72,53 @@ class PostListView(ListView):
     """
     Alternative post list view
     """
+
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 3
-    template_name = 'blog/post/list.html'
+    template_name = "blog/post/list.html"
 
 
 def post_share(request, post_id):
     # Retrieve post by id
-    post = get_object_or_404(
-        Post,
-        id=post_id,
-        status=Post.Status.PUBLISHED
-    )
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     sent = False
 
-    if request.method == 'POST':
+    if request.method == "POST":
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
             # ... send email
-            post_url = request.build_absolute_uri(
-                post.get_absolute_url()
-            )
+            post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = (
                 f"{cd['name']} ({cd['email']}) "
                 f"recommends you read {post.title}"
             )
             message = (
                 f"Read {post.title} at {post_url}\n\n"
-                f"{cd['name']}\'s comments: {cd['comments']}"
+                f"{cd['name']}'s comments: {cd['comments']}"
             )
             send_mail(
                 subject=subject,
                 message=message,
                 from_email=None,
-                recipient_list=[cd['to']]
+                recipient_list=[cd["to"]],
             )
             sent = True
     else:
         form = EmailPostForm()
     return render(
         request,
-        'blog/post/share.html',
-        {
-
-            'post': post,
-            'form': form,
-            'sent': sent
-        }
+        "blog/post/share.html",
+        {"post": post, "form": form, "sent": sent},
     )
 
 
 @require_POST
 def post_comment(request, post_id):
-    post = get_object_or_404(
-        Post,
-        id=post_id,
-        status=Post.Status.PUBLISHED
-    )
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
 
     comment = None
     # A comment was posted
@@ -152,10 +133,26 @@ def post_comment(request, post_id):
 
     return render(
         request,
-        'blog/post/comment.html',
-        {
-            'post': post,
-            'form': form,
-            'comment': comment
-        }
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment},
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body"),
+            ).filter(search=query)
+
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
     )
